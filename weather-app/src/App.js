a85f43cae54fbd7fb16ed6a2069ad310caa2106d
Burak Kutlu <burakutlu0@gmail.com>
2022-10-19 11:10:44 +0300

    app body and its functions are written

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
+import './css/app.css'
+
+const url = "http://api.weatherstack.com/"
+const api = "efaf6840a7d6dc7f81b587c91a469223"
+
+function getWeather(time, location) {
+    fetch(url + time + `?access_key=${api}&query=${location}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
+
+/* EXPERIMENTAL */
+function getUserLocation() {
+    if (navigator.geolocation) {
+        console.log(navigator.geolocation.getCurrentPosition(showPosition));
+    } else {
+        console.log("Geolocation not available.");
+    }
+}
+
+function showPosition(position) {
+    console.log(position);
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [lat, setLat] = useState("null");
+    const [lng, setLng] = useState("null");
+
+    useEffect(() => {
+        getUserLocation();
+    });
+
+
+    return (
+        <main>
+        <section id="weather-main">
+            <div className="weather-top">
+
+            </div>
+            <div className="weather-second">
+
+            </div>
+            <div className="weather-ma">
+
+            </div>
+        </section>
+        </main>
+    );
 }
 
 export default App;
