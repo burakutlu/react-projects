f0b97da7ece1a682ceac42b49e170a43ef0a5ace
Burak Kutlu <burakutlu0@gmail.com>
2022-10-22 16:29:46 +0300

    component main is created and partially written

---
+++ weather-app/src/components/Main.jsx
@@ -0,0 +1,17 @@
+function Main(props) {
+  return (
+    <main>
+      <div className="weather-current">
+        {console.log(props)}
+      </div>
+      <div className="weather-forecast">
+
+      </div>
+      <div className="weather-historical">
+
+      </div>
+    </main>
+  )
+}
+
+export default Main
