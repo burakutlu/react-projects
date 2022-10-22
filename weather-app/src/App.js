5f6b3ea2c07ad637181a092e89467e62bc8a2364
Burak Kutlu <burakutlu0@gmail.com>
2022-10-22 16:29:29 +0300

    main component functions are added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,38 +1,39 @@
 import Header from './components/Header'
+import Main from './components/Main'
 import { useEffect, useState } from 'react';
+
+import './css/style.css'
 import './css/app.css'
 
 const url = "http://api.weatherstack.com/"
 const api = "efaf6840a7d6dc7f81b587c91a469223"
 
-function getWeather(time, location) {
-    fetch(url + time + `?access_key=${api}&query=${location}`)
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-}
-
-
-/* EXPERIMENTAL */
-function getUserLocation() {
-    if (navigator.geolocation) {
-        console.log(navigator.geolocation.getCurrentPosition());
-    } else {
-        console.log("Geolocation not available.");
-    }
-}
 
 function App() {
-    const [lat, setLat] = useState("null");
-    const [lng, setLng] = useState("null");
+  const [lat, setLat] = useState("null");
+  const [lng, setLng] = useState("null");
 
-    useEffect(() => {
-        getUserLocation();
-    });
+  function getWeather(time, location) {
+    fetch(url + time + `?access_key=${api}&query=${location}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+  
+  /* EXPERIMENTAL */
+  function getUserLocation() {
+    if (navigator.geolocation) {
+      console.log(navigator.geolocation.getCurrentPosition());
+    } else {
+      console.log("Geolocation not available.");
+    }
+  }
 
-
-    return (
-        <Header />
-    );
+  return (
+    <>
+      <Header />
+      <Main lat={lat} lng={lng}/>
+    </>
+  );
 }
 
 export default App;
